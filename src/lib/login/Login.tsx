ed0660a718d4bedfd56ea93e4485ab2c0a789c9c
ezzabuzaid <Ezzabuzaid@hotmail.com>
2024-11-19 15:04:54 +0300

    add better login page ui

--- src/lib/login/Login.tsx
+++ src/lib/login/Login.tsx
@@ -98,3 +98,97 @@ export default function Login() {
     </Query>
   );
 }
+
+export function Screen() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="absolute left-0 right-0 top-0 z-10 flex items-center justify-between p-4">
+        <div className="flex items-center">
+          <a href="https://serverize.sh">Serverize</a>
+        </div>
+        <a target="_blank" href="https://serverize.sh/guides">
+          <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input px-4 py-2 text-xs font-medium shadow-sm transition-colors hover:bg-accent hover:text-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+            Documentation
+          </button>
+        </a>
+      </header>
+      <div className="flex flex-1">
+        <div className="flex w-full max-w-2xl flex-col items-center justify-center space-y-8 bg-foreground/5 p-8">
+          <div className="w-[300px] space-y-4 text-center">
+            <h2 className="font-regular text-4xl">Welcome!</h2>
+            <p className="text-sm text-muted-foreground">
+              Sign in to your account
+            </p>
+          </div>
+          <button className="inline-flex h-9 w-[300px] border-spacing-0 items-center justify-center space-y-2 whitespace-nowrap rounded-md border-2 border-gray-700 bg-primary px-4 py-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:border-gray-500 hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth={0}
+              version="1.1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 48 48"
+              enableBackground="new 0 0 48 48"
+              className="mr-2 h-4 w-4"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
+	c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
+	c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              />
+              <path
+                fill="#FF3D00"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
+	C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              />
+              <path
+                fill="#4CAF50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
+	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              />
+              <path
+                fill="#1976D2"
+                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
+	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              />
+            </svg>
+            Continue with Google
+          </button>
+          <p className="w-[300px] max-w-sm text-center text-xs text-muted-foreground">
+            By continuing, you agree to Serverize
+            <a
+              href="https://mem0.ai/privacy-policy"
+              className="mx-0.5 underline"
+            >
+              Terms of Service
+            </a>
+            <span className="mx-0.5">and</span>
+            <a
+              href="https://mem0.ai/privacy-policy"
+              className="mx-0.5 underline"
+            >
+              Privacy Policy
+            </a>
+            , and to receive periodic emails with updates.
+          </p>
+        </div>
+        <div className="hidden flex-1 items-center justify-center border-l border-muted-foreground/20 p-8 lg:flex">
+          <div className="max-w-md space-y-4">
+            <div className="relative flex flex-col gap-6">
+              <div className="z-10 max-w-lg text-3xl">
+                Mem0 is a self-improving memory layer for LLM applications,
+                enabling personalized AI experiences that save costs and delight
+                users.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
